0a91bac84111e7b0b35af38410aff7fb1ec6affe
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-09-03 23:49:13 -0300

    Criando estrutura da página principal

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,11 +1,53 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 import { Wrapper, Container, Main } from "./styles";
 
 import Header from "../../components/Header";
 
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
+
 const Following: React.FC = () => {
+  React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: "PAGE_HEADING",
+        render: () => <View />,
+      },
+      {
+        key: "FOLLOWED_CATEGORIES",
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: "C1", render: () => <View /> },
+
+      {
+        key: "LIVE_CHANNELS",
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: "C2", render: () => <View /> },
+
+      {
+        key: "CONTINUE_WATCHING",
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: "C3", render: () => <View /> },
+
+      {
+        key: "OFFLINE_CHANNELS",
+        render: () => <View />,
+        isTitle: true,
+      },
+      { key: "C4", render: () => <View /> },
+    ];
+  }, []);
+
   return (
     <Wrapper>
       <Container>
