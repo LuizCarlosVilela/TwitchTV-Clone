464ae61f54be5f1e0b03845048c7d9b3ddb422ba
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-09-03 22:18:52 -0300

    Fazendo a página padrão para commingsoon

---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Wrapper, Heading } from "./styles";
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Wrapper>
+      <Heading>Deus é mais</Heading>
+    </Wrapper>
+  );
+};
+
+export default ComingSoon;
