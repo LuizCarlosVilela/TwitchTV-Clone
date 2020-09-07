c5068625b9fee21409aa0f64a57904eed1629a44
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-09-06 21:14:07 -0300

    Update page ComingSoon

--- src/pages/ComingSoon/index.tsx
+++ src/pages/ComingSoon/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 
-import { Wrapper, Heading } from "./styles";
+import { Wrapper, Avatar, OnlineStatus, Heading, HeadingBold } from "./styles";
 
 const ComingSoon: React.FC = () => {
   return (
     <Wrapper>
-      <Heading>Deus é mais</Heading>
+      <TouchableOpacity>
+        <Avatar
+          source={{
+            uri:
+              "https://avatars3.githubusercontent.com/u/57350762?s=460&u=5c28607a6573c3f9ae703ebc9e23c31c916e3ab3&v=4",
+          }}
+          imageStyle={{ borderRadius: 25 }}
+        >
+          <OnlineStatus />
+        </Avatar>
+      </TouchableOpacity>
+      <Heading>
+        <HeadingBold>@</HeadingBold>CarlosVilelal
+      </Heading>
     </Wrapper>
   );
 };
--- src/pages/ComingSoon/styles.ts
+++ src/pages/ComingSoon/styles.ts
@@ -16,8 +16,36 @@ export const Wrapper = styled.SafeAreaView`
   justify-content: center;
   align-items: center;
 `;
+
+export const Avatar = styled.ImageBackground`
+  /* background: ${colors.tag}; */
+
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+`;
+export const OnlineStatus = styled.View`
+  background: ${colors.green};
+  width: 10px;
+  height: 10px;
+  border-radius: 10px;
+
+  border: 2px solid ${colors.primary};
+
+  position: absolute;
+  bottom: 1px;
+  right: 5px;
+`;
 export const Heading = styled.Text`
   color: ${colors.purple};
   font-family: roboto_500;
-  font-size: 30px;
+  font-size: 20px;
+
+  margin: 10px;
+`;
+
+export const HeadingBold = styled.Text`
+  color: ${colors.gray};
+  font-family: roboto_700;
+  font-size: 20px;
 `;
